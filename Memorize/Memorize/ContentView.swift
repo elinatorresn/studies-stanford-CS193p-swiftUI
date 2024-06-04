21fae6437bed1a65ef79be4cc9c804a64d372683
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-06-03 21:47:13 -0300

    lecture 1 - add cardview with isFaceUp

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -5,19 +5,39 @@
 //  Created by Elina Torres on 03/06/24.
 //
 
-import SwiftUI
+import SwiftUI //em todos os arquivos que tem UI (tela) tem q ser importado o swiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world CS193p!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+            }
+        })
+    }
+}
+
+
 
 #Preview {
     ContentView()
