9c3f4ca03838f852a236db10c89bb82d1a29d743
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-06-06 08:24:13 -0300

    add card adjuster

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,14 +10,53 @@ import SwiftUI //em todos os arquivos que tem UI (tela) tem q ser importado o sw
 struct ContentView: View {
     let emojis: [String] = ["👻", "🎃", "🕷️", "💀", "🍭", "😱", "😈", "🧙🏻", "👹", "🕸️", "🙀", "☠️"]
     
+    @State var cardCount: Int = 4
+    
     var body: some View {
+        VStack{
+            cards
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    
+    var cardCountAdjuster: some View {
         HStack{
-            ForEach(emojis.indices, id: \.self) { index in
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        HStack{
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
-        }
-        .foregroundColor(.orange)
-        .padding()
+        }.foregroundColor(.orange)
+        
+    }
+
+    var cardRemover: some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
+    }
+    
+    var cardAdder: some View{
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        }).imageScale(.large)
     }
 }
 
