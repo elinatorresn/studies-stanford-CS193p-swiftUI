b88ee60016ef2325935a2b820e249edf54f082a8
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-06-13 21:21:30 -0300

    add model, view, shuffle and remove theme

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Elina Torres on 13/06/24.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃", "🕷️", "💀", "🍭", "😱", "😈", "🧙🏻", "👹", "🕸️", "🙀", "☠️"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
+            if emojis.indices.contains(pairIndex){
+                return emojis[pairIndex]
+            } else {
+                return "😅"
+            }
+            
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    //MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
