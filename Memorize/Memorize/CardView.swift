8332d7928de2c0daf30fa4b55da5f12100ad4073
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-06-29 16:14:05 -0300

    add cardview

---
+++ Memorize/Memorize/CardView.swift
@@ -0,0 +1,75 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Elina Torres on 25/06/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+    
+    let card: Card
+    
+    init(_ card: Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                Circle()
+                    .opacity(Constants.Pie.opacity)
+                    .overlay(
+                        Text(card.content)
+                            .font(.system(size: Constants.FontSize.largest))
+                            .minimumScaleFactor(Constants.FontSize.scaleFactor)
+                            .multilineTextAlignment(.center)
+                            .aspectRatio(1, contentMode: .fit)
+                    )
+                    .padding(Constants.Pie.inset)
+            
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+    
+    private struct Constants{
+        static let cornerRadius: CGFloat = 12
+        static let lineWidth: CGFloat = 2
+        static let inset: CGFloat = 5
+        struct FontSize {
+            static let largest: CGFloat = 200
+            static let smallest: CGFloat = 10
+            static let scaleFactor = smallest / largest
+        }
+        struct Pie {
+            static let opacity: CGFloat = 0.4
+            static let inset: CGFloat = 5
+        }
+    }
+}
+
+typealias Card = CardView.Card
+#Preview {
+    VStack {
+        HStack {
+            CardView(Card(isFaceUp: true, content: "X", id: "test1"))
+                .aspectRatio(4/3, contentMode: .fit)
+            CardView(Card(content: "X", id: "test1"))
+        }
+        HStack {
+            CardView(Card(isFaceUp: true, isMatched: true, content: "X", id: "test1"))
+            CardView(Card(isMatched: true, content: "X", id: "test1"))
+        }
+    }
+    .padding()
+    .foregroundColor(.green)
+}
